18034bbdc15c4fdefd04b735d881468a850fbf8e
Jacques Briand <57547769+jabrilo@users.noreply.github.com>
2026-04-18 15:20:03 -0400

    Clean UI(.css) files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,29 +2,6 @@ import { useState, useEffect } from "react"
 import "./App.css"
 
 function App() {
-    const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch(`${import.meta.env.VITE_BASE_API_URL}/`)
-                const payload = await res.json()
-                setData(payload)
-
-            } catch (err){
-                setError(err.message)
-            } finally {
-                setLoading(false)
-            }
-        }
-        fetchData()
-    }, [])
-
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>Error: {error}</p>
-
     return (
         <>
             <div class="chat-container">
